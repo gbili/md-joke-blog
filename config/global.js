5e09a2165d703e919951299eeec4a6b465ddb2f9
Guillermo Pages <public.guillermo@gmail.com>
2019-06-26 15:18:09 +0200

    fix: import from ./dist instead of ./src to fix build

--- config/global.js
+++ config/global.js
@@ -1,8 +1,8 @@
 import { resolve } from 'path';
-import BlogPostRoute from '../src/route/BlogPostRoute';
-import BlogHomeRoute from '../src/route/BlogHomeRoute';
-import StaticFileRoute from '../src/route/StaticFileRoute';
-import NotFoundRoute from '../src/route/NotFoundRoute';
+import BlogPostRoute from '../dist/route/BlogPostRoute';
+import BlogHomeRoute from '../dist/route/BlogHomeRoute';
+import StaticFileRoute from '../dist/route/StaticFileRoute';
+import NotFoundRoute from '../dist/route/NotFoundRoute';
 import postList from './post-list';
 import staticFileList from './static-file-list';
 import dir from './dir';
@@ -16,7 +16,6 @@ const config = {
   },
   ...dir,
   routes: [
-    // post route
     {
       path: '/', 
       route: BlogPostRoute,
@@ -24,7 +23,6 @@ const config = {
         validPostSlugList,
       }
     },
-    // main 
     {
       path: '/',
       route: BlogHomeRoute,
