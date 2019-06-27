3924f20ca9b436b6f2025e412ed112e2e9943d27
Guillermo Pages <public.guillermo@gmail.com>
2019-06-27 14:35:53 +0200

    fix: change project initials

--- config/global.js
+++ config/global.js
@@ -72,7 +72,7 @@ config.routes = [
   },
 ];
 
-if (process.env.MJB_ENV === 'production') {
+if (process.env.MTB_ENV === 'production') {
   config.mdBlogPostsDir = userConfig.mdBlogPostsDir || `${__dirname}/../../../content`
   config.staticFilesDir = userConfig.staticFilesDir || `${__dirname}/../../../static`
   config.viewData = userConfig.viewData || config.viewData;
